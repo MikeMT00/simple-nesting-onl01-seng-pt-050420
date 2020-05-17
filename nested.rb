a6021d8c7adf1d7ecb7ea98bc42f6ea88fc2a898
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-17 21:39:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
      }
 
 
-		 programmer_hash = {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
+		 hopper = {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]}
 end
 
 def alan_kay_is_known_for
